4590bee23d9f4989fa7f7c82938e7aee760c028e
Justin <justinmmlee@gmail.com>
2024-07-24 18:46:48 -0500

    changes

---
+++ README.md
@@ -0,0 +1,9 @@
+
+ <h3 align="center">Next.JS Portfolio for Justin Lee</h3>
+
+   <div align="center">
+     Portfolio containing work experience thanks to <a href="https://github.com/adrianhajdin/portfolio" target="_blank"><b>JavaScript Mastary</b></a>.
+    </div>
+</div>
+
+Build with Next.JS, Typescript, Tailwind.CSS, and React Elements using Aceternity UI 
---
+++ data/index.ts
@@ -0,0 +1,182 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize collaboration, communication, and teamwork.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "./ples.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm flexible with time zone and communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My skills and assets",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionate about technology and skilled in software creation.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "./grid.svg",
+    spareImg: "./b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Working on Cloud Development & Deployment",
+    description: "Currently...",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "./b5.svg",
+    spareImg: "./grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Dranima",
+    des: "This is a short passion project made in Unity with loose mechanics inspired by the Pokemon Series. This project is longer in development.",
+    img: "./chube_icon.png",
+    iconLists: ["./unity.svg", "./git_icon.svg", "./c-sharp.svg"],
+    link: "https://justinl11.itch.io/dranima",
+  },
+  {
+    id: 2,
+    title: "Project CodeX",
+    des: "Short Visual Novel Demo built in Renpy",
+    img: "./tome.png",
+    iconLists: ["./python_icon.svg", "./vscode_icon.svg"],
+    link: "https://justinl11.itch.io/codex",
+  },
+  {
+    id: 3,
+    title: "Chuba Clicker",
+    des: "Simple Clicker Web Game made for a Final Project, using MySQL, Jquery, and Javascript",
+    img: "./chubaclick.png",
+    iconLists: ["./js_icon.svg", "./mysql.svg", "./jq.svg"],
+    link: "N/A",
+  },
+  {
+    id: 4,
+    title: "PassGen",
+    des: "PassGen is a user-friendly tool designed to create secure and unique passwords. The software generates unique, random passwords that are challenging for hackers to crack, thereby enhancing user account security. Users have the freedom to customize their passwords based on their preferences. They can specify the password's length, choose whether to include uppercase letters or numbers, and even incorporate personal text, such as a favorite movie title or a meaningful phrase.",
+    img: "./passgen.png",
+    iconLists: ["./js_icon.svg", "./mysql.svg", "./fig.svg", "./git_icon.svg", "./gitlab.svg"],
+    link: "https://github.com/ZakFaarah/PassGen-main",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "“I have had the joy of collaborating with Justin in a variety of projects both big and small using a variety of different strategies and frameworks. He has an excellent work ethic and makes sure to not only produce results in a timely manner but makes sure his work is as effective and efficient as possible. Justin is flexible and a quick learner able to tackle new problems with quality solutions. Justin is an excellent addition for anyone searching for a flexible developer who can excel not only as a developer but as an overall team member.",
+    name: "Drew Friend",
+    title: "Software Engineer at Redwood Logistics",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Family Lead / Organizer",
+    desc: "Spearheaded a team of four to lead student mentors, fostering exceptional teamwork and cohesion.",
+    className: "md:col-span-2",
+    thumbnail: "./teamwork.png",
+  },
+  {
+    id: 2,
+    title: "Sales Associate",
+    desc: "Led a team in managing BnB products, surpassing daily sales targets and driving revenue growth.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./graph.png",
+  },
+  {
+    id: 3,
+    title: "Barista",
+    desc: "Actively promoted specials and expertly crafted Starbucks products to drive sales.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "./barista.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "./git.svg",
+  },
+  {
+    id: 3,
+    img: "./link.svg",
+  },
+];
